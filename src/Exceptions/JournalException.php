cc7d8614a618edabf6e7b9c155db6f8be4e648b2
Juriy Panasevich <elfelrandcor@gmail.com>
2015-04-06 17:26:13 +0300

    Exceptions

---
+++ src/Exceptions/JournalException.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author Juriy Panasevich <juriy.panasevich@gmail.com>
+ */
+
+namespace JuriyPanasevich\Logger\Exceptions;
+
+use Exception;
+
+class JournalException extends Exception {
+
+}
